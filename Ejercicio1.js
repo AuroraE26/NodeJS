6600b8b46f592aba3f645c63e5bd54f32390a9f0
Aurora <unefleur26@gmail.com>
2021-11-11 12:10:21 -0600

    Ejercicio de primera clase

---
+++ Ejercicio1.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 5;
+
+function numMayor(n1,n2){
+    let res=n1/n2;
+    if(b==0){
+        console.log("Error");
+    }else{
+        console.log("El resultado de la division es: "+res);
+    }
+}
+
+numMayor(a,b);
